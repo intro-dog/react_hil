da88a810a794219718b98965a9457023fed58e78
intro-dog <metvlad3@gmail.com>
2024-06-27 12:52:27 +0300

    added homework_2

---
+++ homework_2/src/App.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react"
+import "./App.css"
+import Tasks from "./components/Tasks/Tasks"
+import {
+  STATUS_DONE,
+  STATUS_IN_PROGRESS,
+  STATUS_TODO,
+} from "./constants/constants"
+import { tasksData } from "./data/tasksData"
+
+function App() {
+  const [tasks, setTasks] = useState(tasksData)
+
+  const changeStatus = (id, newStatus) => {
+    setTasks((prevState) =>
+      prevState.map((task) =>
+        task.id === id ? { ...task, status: newStatus } : task
+      )
+    )
+  }
+
+  const deleteTask = (id) => {
+    setTasks((prevState) => prevState.filter((task) => task.id !== id))
+  }
+
+  return (
+    <div className="container">
+      <div className="tasks__column">
+        <h2>To Do</h2>
+
+        {tasks
+          .filter((task) => task.status === STATUS_TODO)
+          .map((task) => (
+            <Tasks key={task.id} task={task} changeStatus={changeStatus} />
+          ))}
+      </div>
+      <div className="tasks__column">
+        <h2>In Progress</h2>
+        {tasks
+          .filter((task) => task.status === STATUS_IN_PROGRESS)
+          .map((task) => (
+            <Tasks key={task.id} task={task} changeStatus={changeStatus} />
+          ))}
+      </div>
+      <div className="tasks__column">
+        <h2>Done</h2>
+        {tasks
+          .filter((task) => task.status === STATUS_DONE)
+          .map((task) => (
+            <Tasks
+              key={task.id}
+              task={task}
+              changeStatus={changeStatus}
+              deleteTask={deleteTask}
+            />
+          ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ homework_2/src/components/Tasks/Tasks.jsx
@@ -0,0 +1,41 @@
+import {
+  STATUS_DONE,
+  STATUS_IN_PROGRESS,
+  STATUS_TODO,
+} from "../../constants/constants.js"
+export default function Tasks({ task, changeStatus, deleteTask }) {
+  return (
+    <div className="tasks">
+      <h3 className="tasks__title">{task.title}</h3>
+      {task.status === STATUS_TODO && (
+        <button
+          className="tasks__button"
+          onClick={() => changeStatus(task.id, STATUS_IN_PROGRESS)}
+        >
+          In Progress
+        </button>
+      )}
+      {task.status === STATUS_IN_PROGRESS && (
+        <>
+          <button
+            className="tasks__button"
+            onClick={() => changeStatus(task.id, STATUS_TODO)}
+          >
+            To Do
+          </button>
+          <button
+            className="tasks__button"
+            onClick={() => changeStatus(task.id, STATUS_DONE)}
+          >
+            Done
+          </button>
+        </>
+      )}
+      {task.status === STATUS_DONE && (
+        <button className="tasks__button" onClick={() => deleteTask(task.id)}>
+          To Archive
+        </button>
+      )}
+    </div>
+  )
+}
